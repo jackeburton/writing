958caaf2275f6eb3af5f234f3d0de5209631fafc
Jack Burton <jackeburton97@gmail.com>
2024-01-15 19:09:09 +0000

    adding refresh to server and minor html tweaks

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+import threading
+
+LENGTH_LIMIT = 10_000
+TIME_LIMIT = 5
 
 app = Flask(__name__)
 
@@ -9,6 +13,26 @@ def app_home_page():
 @app.route('/hello')
 def hello():
     return 'hello from htmx'
+
+@app.route('/post', methods=['GET','POST'])
+def post():
+    # print(request.form)
+    return handler(request)
+ 
+
+def handler(input):
+    if input.content_length > LENGTH_LIMIT:
+        print('length limit reached')
+        return false
+    input_text = request.form.get('text_box') 
+    # start a timer, if after TIME_LIMIT seconds this code is not called again do another fucntion
+    timer = threading.Timer(TIME_LIMIT, lambda: on_timeout(input_text))
+    timer.start()
+    return render_template('text_box.html')
+
+def on_timeout(post_text):
+    print(f"has timedout: {post_text}")
+    # clear the text area
 
 if __name__ == '__main__':
     app.run(debug=True)
